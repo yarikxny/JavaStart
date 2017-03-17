1326222eadd2537f3ed2c06fcd83a9f9208c1f32
eugeneiarosh-pc\eugene.iarosh <evgeniy.iarosh@gmail.com>
2017-03-17 18:13:38 +0200

    Added constants notice

---
+++ src/notes/Constants.java
@@ -0,0 +1,8 @@
+package notes;
+
+/**
+ * Created by eugene.iarosh on 3/17/2017.
+ */
+public class Constants {
+    public static final int YEARSOLD = 29;
+}
---
+++ src/notes/Notes.java
@@ -0,0 +1,8 @@
+package notes;
+
+/**
+ * Created by eugene.iarosh on 3/17/2017.
+ */
+public class Notes {
+    int yearsOld = Constants.YEARSOLD;
+}
